adf4707471d43e03283848e71d714cd84d375384
Саша Шевченко <casiop8@gmail.com>
2021-02-09 12:52:31 +0200

    GUI button on click animations

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MainActivity that = this;
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                that.findViewById(R.id.button79).setOnClickListener(this::testClick);
+                that.findViewById(R.id.editTextNumberDecimal).setOnClickListener(this::textClick);
+            }
+            public void testClick(View View)
+            {
+                Toast.makeText(that, "You god damn right!", Toast.LENGTH_SHORT).show();
+            }
+            public void textClick(View view)
+            {
+                InputMethodManager imm = (InputMethodManager) that.getSystemService(Activity.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+            }
+        };
+        runnable.run();
     }
 }
